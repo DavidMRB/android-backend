5839507aa6ef440b76809eefbfad262ecf9521e5
DavidMRB <davidrangelbaez@gmail.com>
2025-11-26 02:06:10 -0500

    Fix JWT success handler formatting

--- src/EventListener/JWTSuccessHandler.php
+++ src/EventListener/JWTSuccessHandler.php
@@ -1,19 +1,24 @@
 <?php
 
-// src/EventListener/JWTSuccessHandler.php
 namespace App\EventListener;
 
 use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class JWTSuccessHandler
 {
-public function onAuthenticationSuccess(AuthenticationSuccessEvent $event): void
-{
-$user = $event->getUser();
-$data = $event->getData();
+    public function onAuthenticationSuccess(AuthenticationSuccessEvent $event): void
+    {
+        $user = $event->getUser();
+        $data = $event->getData();
 
-// Agrega solo el ID del usuario
-$data['user_id'] = $user->getId();
-$event->setData($data);
-}
+        // Add user info to the response
+        $data['user'] = [
+            'id' => $user->getId(),
+            'email' => $user->getEmail(),
+            'name' => $user->getName(),
+        ];
+
+        $event->setData($data);
+    }
 }
